b62209c9eebf43642a8e4e3541138f6933782ea1
Gookun <75612656+Gookun@users.noreply.github.com>
2022-07-01 17:11:40 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,119 @@
+import yfinance as yf
+import numpy as np
+import pandas as pd
+import pandas_datareader as data
+import matplotlib.pyplot as plt
+from sklearn.preprocessing import MinMaxScaler 
+from keras.models import load_model
+import streamlit as st
+from PIL import Image
+
+#styling
+plt.style.use('dark_background')
+img = Image.open('icon.png')
+st.set_page_config(page_title='Stock Prediction Web-App',page_icon=img)
+hide_menu_style = """
+		<style>
+		#MainMenu {visibility: hidden;}
+		footer {visibility:hidden;}
+		</style>
+		"""
+st.markdown(hide_menu_style, unsafe_allow_html=True)
+
+
+start = '2010-01-01'
+end ='2022-07-01'
+
+#heading
+heading = st.container()
+with heading.container():
+	st.title('Stock Prediction')
+
+	user_input = st.text_input('Enter Stock Ticker', 'AAPL')
+	df = data.DataReader(user_input,'yahoo', start, end)
+
+	#Describing Data
+	st.subheader('Data from 2010 - 2022')
+	st.write(df.describe())
+
+
+#Visualizations
+
+option = st.selectbox('Select',
+	['Closing Price vs Time chart','Closing Price vs Time chart with 100MA','Closing Price vs Time chart with 100MA & 200MA'])
+
+contain = st.empty()
+if 'Closing Price vs Time chart' in option:
+	contain.empty()
+	with contain.container():
+		st.subheader('Closing Price vs Time chart')
+		fig = plt.figure(figsize = (12,6))
+		plt.plot(df.Close)
+		st.pyplot(fig)
+
+if 'Closing Price vs Time chart with 100MA' in option:
+	contain.empty()
+	with contain.container():
+		st.subheader('Closing Price vs Time chart with 100MA')
+		ma100 = df.Close.rolling(100).mean()
+		fig = plt.figure(figsize = (12,6))
+		plt.plot(ma100)
+		plt.plot(df.Close)
+		st.pyplot(fig)
+
+
+if 'Closing Price vs Time chart with 100MA & 200MA' in option:
+	contain.empty()
+	with contain.container():
+		st.subheader('Closing Price vs Time chart with 100MA & 200MA')
+		ma100 = df.Close.rolling(100).mean()
+		ma200 = df.Close.rolling(200).mean()
+		fig = plt.figure(figsize = (12,6))
+		plt.plot(ma100,'r')
+		plt.plot(ma200, 'g')
+		plt.plot(df.Close, 'b')
+		st.pyplot(fig)
+
+
+#spliting data for testing
+end= st.container()
+with end.container():
+	data_training = pd.DataFrame(df['Close'][0:int(len(df)*0.70)])
+	data_testing = pd.DataFrame(df['Close'][int(len(df)*0.70): int(len(df))])
+
+	scaler = MinMaxScaler(feature_range=(0,1))
+	data_training_array = scaler.fit_transform(data_training)
+
+
+	#loading model
+	model = load_model('stock_prediction_model.h5')
+
+	#Testing
+	past_100_days = data_training.tail(100)
+	final_df = past_100_days.append(data_testing, ignore_index=True)
+	input_data = scaler.fit_transform(final_df)
+
+	x_test = []
+	y_test = []
+
+	for i in range(100, input_data.shape[0]):
+	  x_test.append(input_data[i-100: i])
+	  y_test.append(input_data[i, 0])
+
+	x_test, y_test = np.array(x_test), np.array(y_test)
+	y_predicted = model.predict(x_test)
+
+	scalerValue = scaler.scale_ 
+	scale_factor = 1/scalerValue[0]
+	y_predicted = y_predicted * scale_factor
+	y_test = y_test * scale_factor
+
+	#Final Graph
+	st.subheader('Predictions vs Original')
+	fig2 = plt.figure(figsize=(12,6))
+	plt.plot(y_test,'b',label = 'Original Price')
+	plt.plot(y_predicted, 'r', label = 'predicted ')
+	plt.xlabel('Time')
+	plt.ylabel('Price')
+	plt.legend()
+	st.pyplot(fig2)
